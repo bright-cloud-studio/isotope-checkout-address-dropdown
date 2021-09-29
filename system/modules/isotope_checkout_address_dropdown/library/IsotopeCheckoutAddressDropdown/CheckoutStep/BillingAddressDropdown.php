9ca14b3cc72e6bf6910e3760d0b65bc552655094
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-29 14:26:21 -0400

    Update BillingAddressDropdown.php

--- system/modules/isotope_checkout_address_dropdown/library/IsotopeCheckoutAddressDropdown/CheckoutStep/BillingAddressDropdown.php
+++ system/modules/isotope_checkout_address_dropdown/library/IsotopeCheckoutAddressDropdown/CheckoutStep/BillingAddressDropdown.php
@@ -62,7 +62,7 @@ class BillingAddressDropdown extends BillingAddress implements IsotopeCheckoutSt
                     'mandatory'   => true,
                     'options'     => $arrOptions,
                     'value'       => $varValue,
-                    'onclick'     => "Isotope.toggleAddressFields(this, '" . $this->getStepClass() . "_new');",
+                    'onchange'     => "Isotope.toggleAddressFields(this, '" . $this->getStepClass() . "_new');",
                     'storeValues' => true,
                     'tableless'   => true,
                 ]
