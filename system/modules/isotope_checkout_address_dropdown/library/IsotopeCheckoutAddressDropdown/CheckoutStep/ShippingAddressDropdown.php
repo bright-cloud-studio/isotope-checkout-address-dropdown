8862d858dee4cf2fd2c7a9ddcefa6c3ef57a590c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-29 14:21:36 -0400

    Update ShippingAddressDropdown.php

--- system/modules/isotope_checkout_address_dropdown/library/IsotopeCheckoutAddressDropdown/CheckoutStep/ShippingAddressDropdown.php
+++ system/modules/isotope_checkout_address_dropdown/library/IsotopeCheckoutAddressDropdown/CheckoutStep/ShippingAddressDropdown.php
@@ -95,9 +95,6 @@ class ShippingAddressDropdown extends ShippingAddress implements IsotopeCheckout
         if ($varValue !== '0') {
             $this->Template->style = 'display:none;';
         }
-	if ($varValue == '0') {
-            $this->Template->style = '';
-        }
 
         $objAddress = $this->getAddressForOption($varValue, $blnValidate);
 
