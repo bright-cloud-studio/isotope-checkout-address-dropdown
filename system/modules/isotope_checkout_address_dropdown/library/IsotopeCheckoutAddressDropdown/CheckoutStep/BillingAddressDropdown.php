847ba1ce16cd4f3fd5c3e7eca72e30288f6fadef
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-29 13:35:06 -0400

    Update and rename BillingAddress.php to BillingAddressDropdown.php

---
+++ system/modules/isotope_checkout_address_dropdown/library/IsotopeCheckoutAddressDropdown/CheckoutStep/BillingAddressDropdown.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * FedEx Shipping Isotope eCommerce and Contao CMS
+ *
+ * Copyright (C) 2020 Andrew Stevens Consulting
+ *
+ * @package    isotope_shipping_fedex
+ * @link       https://andrewstevens.consulting
+ */
+
+
+
+namespace IsotopeCheckoutAddressDropdown\CheckoutStep;
+
+//use IsotopeFedEx\Model\Shipping\FedEx as FedExShippingModel;
+//use IsotopeFedEx\AddressValidator;
+
+use Contao\StringUtil;
+
+use Isotope\CheckoutStep\BillingAddress;
+use Isotope\Interfaces\IsotopeCheckoutStep;
+use Isotope\Interfaces\IsotopeProductCollection;
+use Isotope\Isotope;
+use Isotope\Model\Address as AddressModel;
+use Isotope\Module\Checkout;
+use Model\Registry;
+
+/**
+ * BillingAddressDropdown checkout step lets the user enter a shipping address
+ */
+class BillingAddressDropdown extends BillingAddress implements IsotopeCheckoutStep
+{
+
+	 /**
+	* Generate address options and return it as HTML string
+	*
+	* @param bool $blnValidate
+	*
+	* @return string
+	*/
+	protected function generateOptions($blnValidate = false)
+    {
+        $strBuffer  = '';
+        $varValue   = '0';
+        $arrOptions = $this->getAddressOptions();
+
+        if (0 !== \count($arrOptions)) {
+            foreach ($arrOptions as $option) {
+                if ($option['default']) {
+                    $varValue = $option['value'];
+                }
+            }
+
+            $strClass  = $GLOBALS['TL_FFL']['radio'];
+
+            /** @var \Widget $objWidget */
+            $objWidget = new $strClass(
+                [
+                    'id'          => $this->getStepClass(),
+                    'name'        => $this->getStepClass(),
+                    'mandatory'   => true,
+                    'options'     => $arrOptions,
+                    'value'       => $varValue,
+                    'onclick'     => "Isotope.toggleAddressFields(this, '" . $this->getStepClass() . "_new');",
+                    'storeValues' => true,
+                    'tableless'   => true,
+                ]
+            );
+
+            // Validate input
+            if ($blnValidate) {
+                $objWidget->validate();
+
+                if ($objWidget->hasErrors()) {
+                    $this->blnError = true;
+                } else {
+                    $varValue = (string) $objWidget->value;
+                }
+            } elseif ($objWidget->value != '') {
+                \Input::setPost($objWidget->name, $objWidget->value);
+
+                $objValidator = clone $objWidget;
+                $objValidator->validate();
+
+                if ($objValidator->hasErrors()) {
+                    $this->blnError = true;
+                }
+            }
+
+            $strBuffer .= $objWidget->parse();
+        }
+
+        if ($varValue !== '0') {
+            $this->Template->style = 'display:none;';
+        }
+
+        $objAddress = $this->getAddressForOption($varValue, $blnValidate);
+
+        if (null === $objAddress || !Registry::getInstance()->isRegistered($objAddress)) {
+            $this->blnError = true;
+        }  elseif ($blnValidate) {
+            $this->setAddress($objAddress);
+        }
+
+        //return $strBuffer;
+	return 'ASDF';
+    }
+	
+}
