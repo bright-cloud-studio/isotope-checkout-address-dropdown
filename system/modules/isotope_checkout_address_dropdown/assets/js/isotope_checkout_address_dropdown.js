a6bd58077260758a95e2ffe500a999b495d4422f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-30 14:45:16 -0400

    Update isotope_checkout_address_dropdown.js

--- system/modules/isotope_checkout_address_dropdown/assets/js/isotope_checkout_address_dropdown.js
+++ system/modules/isotope_checkout_address_dropdown/assets/js/isotope_checkout_address_dropdown.js
@@ -1,1 +1,9 @@
-console.log("This script is getting on page");
+// when making selection with select, change the create new address button
+$('#ctrl_billingaddressdropdown').on('change', function() {
+  if(this.value == 0)
+  {
+     //check button
+    $("#opt_billingaddress_71").prop("checked", true);
+  }
+});
+
