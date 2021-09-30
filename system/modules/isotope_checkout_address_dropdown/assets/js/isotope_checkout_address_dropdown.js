40fe3dbe6912378c2e2da3bf738db6ba74e549bd
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-30 14:49:14 -0400

    Update isotope_checkout_address_dropdown.js

--- system/modules/isotope_checkout_address_dropdown/assets/js/isotope_checkout_address_dropdown.js
+++ system/modules/isotope_checkout_address_dropdown/assets/js/isotope_checkout_address_dropdown.js
@@ -2,8 +2,20 @@
 $('#ctrl_billingaddressdropdown').on('change', function() {
   if(this.value == 0)
   {
-     //check button
     $("#opt_billingaddress_71").prop("checked", true);
+  } else {
+    $("#opt_billingaddress_71").prop("checked", false);
+  }
+});
+
+
+// when making selection with select, change the create new address button
+$('#ctrl_shippingaddressdropdown').on('change', function() {
+  if(this.value == 0 || this.value == -1)
+  {
+    $("#opt_shippingaddress_71").prop("checked", true);
+  } else {
+    $("#opt_shippingaddress_71").prop("checked", false);
   }
 });
 
