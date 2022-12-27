d8020d82adbdac1079dd7d9c9f52c92d3f7a6ee1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-27 15:29:28 -0500

    Update isotope_checkout_address_dropdown.js

--- system/modules/isotope_checkout_address_dropdown/assets/js/isotope_checkout_address_dropdown.js
+++ system/modules/isotope_checkout_address_dropdown/assets/js/isotope_checkout_address_dropdown.js
@@ -1,39 +1,44 @@
-// when making selection with select, change the create new address button
+// when changing the billing address dropdown
 $('#ctrl_billingaddressdropdown').on('change', function() {
-  if(this.value == 0)
-  {
-    $("#opt_billingaddress_71").prop("checked", true);
-  } else {
-    $("#opt_billingaddress_71").prop("checked", false);
-  }
+
+    // if our value is "Create New Address"
+    if(this.value === 0)
+    {
+        // tick our radio and trigger our onlick event
+        $("#opt_billingaddress_71").prop("checked", true);
+        $("#opt_billingaddress_71")[0].onclick();
+    } else {
+        $("#opt_billingaddress_71").prop("checked", false);
+    }
 });
 
-
-// when making selection with select, change the create new address button
+// when changing the shipping address dropdown
 $('#ctrl_shippingaddressdropdown').on('change', function() {
-  if(this.value == 0)
-  {
-    $("#opt_shippingaddress_71").prop("checked", true);
-  } else {
-    $("#opt_shippingaddress_71").prop("checked", false);
-  }
+    
+    // if our value is "Create New Address"
+    if(this.value === 0)
+    {
+        // tick our radio and trigger our onclick event
+        $("#opt_shippingaddress_71").prop("checked", true);
+        $("#opt_shippingaddress_71")[0].onclick();
+    } else {
+        $("#opt_shippingaddress_71").prop("checked", false);
+    }
 });
 
-
-// when checking billing address radio, make select match
+// make our billing select match our radio button
 $('input:radio[name="billingaddress"]').change(
     function(){
         if ($(this).is(':checked')) {
-            // append goes here
-            $("#ctrl_billingaddressdropdown").val("0").change();
-        }
-    });
+        $("#ctrl_billingaddressdropdown").val("0").change();
+    }
+});
 
-// when checking shipping address radio, make select match
+// make our shipping select match our radio button
 $('input:radio[name="shippingaddress"]').change(
     function(){
-        if ($(this).is(':checked')) {
-            // append goes here
-            $("#ctrl_shippingaddressdropdown").val("0").change();
-        }
-    });
+    if ($(this).is(':checked')) {
+        $("#ctrl_shippingaddressdropdown").val("0").change();
+        $("#opt_shippingaddress_71")[0].onclick();
+    }
+});
